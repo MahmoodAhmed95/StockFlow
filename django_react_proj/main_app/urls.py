25832a2205ce4d8858d4021c51e8bba37c4b2fe6
hamzashana1 <Hamza@zod-alkhair.com>
2023-09-10 14:17:40 +0300

    added homepage and navbar

---
+++ django_react_proj/main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+path('', views.home, name='home'),
+path('about/', views.about, name='about'),
+path('category/', views.category, name='category'),
+path('product/', views.product, name='product'),
+path('purchase/', views.purchase, name='purchase'),
+path('sale/', views.sale, name='sale'),
+path('customer/', views.customer, name='customer'),
+path('vendor/', views.vendor, name='vendor'),
+]
