6885f0fb7e4f1261280acd259fed37e0b3c11ae4
hamzashana1 <Hamza@zod-alkhair.com>
2023-09-10 14:17:40 +0300

    added homepage and navbar

--- django_react_proj/main_app/views.py
+++ django_react_proj/main_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def category(request):
+  return render(request, 'main_app/category.html')
+
+def product(request):
+  return render(request, 'main_app/product.html')
+
+def purchase(request):
+  return render(request, 'main_app/purchase.html')
+
+def sale(request):
+  return render(request, 'main_app/sale.html')
+
+def customer(request):
+  return render(request, 'main_app/customer.html')
+
+def vendor(request):
+  return render(request, 'main_app/vendor.html')
+
+
