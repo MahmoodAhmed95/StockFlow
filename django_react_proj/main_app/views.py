c985aaabfec2055dc4ce52dcc6da89e3c72c7c5e
hamzashana1 <Hamza@zod-alkhair.com>
2023-09-11 09:51:47 +0300

    added products to category

--- django_react_proj/main_app/views.py
+++ django_react_proj/main_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import render, get_object_or_404
+from .models import Categories, Product
 
 # Define the home view
 def home(request):
@@ -10,10 +10,16 @@ def about(request):
   return render(request, 'about.html')
 
 def category(request):
-  categories = Category.objects.all()
+  categories = Categories.objects.all()
   return render(request, 'main_app/category.html',{
 'categories': categories
   })
+
+def categories_detail(request, category_id):
+    category = get_object_or_404(Categories, id=category_id)
+    products = Product.objects.filter(categoryId=category)
+    return render(request, 'categories/detail.html', {'category': category, 'products': products})
+
 
 def product(request):
   return render(request, 'main_app/product.html')
